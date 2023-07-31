16777ce6acc0b4bb71b5634500c0a6f57a58bde1
pomazkovanton <vitrumbeta@gmail.com>
2023-07-31 22:52:47 +0800

    fix: фикс конфига для деплоя

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '.env.deploy' });
+require("dotenv").config({ path: ".env.deploy" });
 
 const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY } =
   process.env;
@@ -6,8 +6,8 @@ const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY } =
 module.exports = {
   apps: [
     {
-      name: 'mesto-backend',
-      script: './dist/app.js',
+      name: "mesto-backend",
+      script: "dist/app.js",
     },
   ],
   deploy: {
@@ -17,8 +17,9 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
-      'post-deploy': 'cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      "pre-deploy-local": `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      "post-deploy":
+        "cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production",
     },
   },
 };
